cd7548f6c33ed4871db9433f1fdcba4e70a04deb
AlexunderJ <pozdrower@wp.pl>
2020-11-26 05:25:04 +0100

    form validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,25 +5,35 @@ import classes from './Input.module.css';
 export default function Input(props) {
 
     let inputElement = null;
+    const inputClasses = [classes.inputElement];
+
+    if(props.invalid && props.shouldValidate && props.touched){
+      inputClasses.push(classes.Invalid)  
+    }
+
+    let validationError = null;
+if (props.invalid && props.touched) {
+validationError = <p className={classes.ValidationError}> You have to input Your valid: <br/> {props.errorMessage}</p>;
+}
 
     switch(props.elementType){
         case ('input'):
             inputElement = <input 
-            className={classes.inputElement} 
+            className={inputClasses.join(' ')} 
             {...props.elementConfig} 
             value={props.value}
             onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea 
-            className={classes.inputElement} 
+            className={inputClasses.join(' ')} 
             {...props.elementConfig} 
             value={props.value}
             onChange={props.changed}/>;
             break;
         case ('select'):
     inputElement = (<select 
-        className={classes.inputElement} 
+        className={inputClasses.join(' ')} 
     value={props.value}
     onChange={props.changed}> 
     {props.elementConfig.options.map(option=>
@@ -43,6 +53,7 @@ export default function Input(props) {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     )
 }
